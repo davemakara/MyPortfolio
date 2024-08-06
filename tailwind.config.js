fc583157c8bda85e6a999d9ca744307c2623a5ee
davidmakarovv <davidmakarr@gmail.com>
2024-08-06 23:33:20 +0400

    installed tailwind and builing up the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1340px",
+      xxl: "1640px",
+    },
+    colors: {
+      black: "#000000",
+      white: "#ffffff",
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
