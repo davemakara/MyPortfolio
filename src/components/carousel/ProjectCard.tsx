870be74a06e24c82732b9bb1603201cdc6ca0543
davidmakarovv <davidmakarr@gmail.com>
2024-08-16 01:49:53 +0400

    projectCard modified

--- src/components/carousel/ProjectCard.tsx
+++ src/components/carousel/ProjectCard.tsx
@@ -24,7 +24,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
         <div className="absolute top-3 md:top-5 right-3 md:right-5 text-4xl md:text-6xl lg:text-5xl xxl:text-7xl text-yellow shadow-lg shadow-black rounded-full overflow-hidden">
           <MdOutlineSwipeRight className=" animate-rotate-right" />
         </div>
-        <div className="absolute opacity-0 group-hover:opacity-100 transition-opacity duration-500 bottom-0 bg-opacity-90 w-full h-3/5 bg-black p-3 sm:p-6 md:p-8 xxl:p-12">
+        <div className="absolute top-full group-hover:-translate-y-full transition-transform duration-300 bg-opacity-90 w-full h-3/5 bg-black p-3 sm:p-6 md:p-8 xxl:p-12">
           <p className="text-base sm:text-2xl xl:text-2xl xxl:text-5xl font-normal mb-2 lg:mb-3 xxl:mb-10">
             {project.name}
           </p>
